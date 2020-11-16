d89c47b1c6d1428d93ba9a842bd7280b868e3075
Jeff <jlgranof@icloud.com>
2020-11-16 00:06:46 -0600

    refactored css for landing page

--- client/src/Components/PokemonPage/PokemonListing.js
+++ client/src/Components/PokemonPage/PokemonListing.js
@@ -1,50 +1,22 @@
 import React from 'react';
-
-// Material UI Imports
-import { Card, makeStyles, CardActionArea, CardMedia, Typography } from '@material-ui/core'
+import styles from './PokemonListing.module.scss'
 import { NavLink } from 'react-router-dom';
 
-const useStyles = makeStyles({
-    root: {
-      width: 250,
-      margin: 15,
-      border: '1px solid black',
-      boxShadow: '0 10px 10px 10px #bfbfbf',
-      borderRadius: '10%'
-    },
-    media: {
-      height: 250
-    },
-    info: {
-      textAlign: 'center'
-    },
-    name: {
-      textShadow: '5px 5px #bfbfbf',
-      color: 'black',
-      textDecoration: 'none'
-    }
-  });
-
-
   const PokemonListing = (props) => {
-    const classes = useStyles();
-
-
     return (
-      <Card className={classes.root} >
-          <CardActionArea >
+      <div className={styles.root} >
               <NavLink to={`/pokemon/${props.pokemon.id}`} >
-                  <CardMedia
-                      className={classes.media}
-                       image={props.pokemon.official_artwork}
-                  />
+                    <img
+                        className={styles.media}
+                        src={props.pokemon.official_artwork}
+                        alt={props.pokemon.name}
+                    />
               </NavLink>
-                  <div className={classes.info}>
-                    <h2 className={classes.name}>#{props.pokemon.id}</h2>
-                    <h1 className={classes.name}>{props.pokemon.name}</h1>
+                  <div className={styles.info}>
+                    <h2 className={styles.name}>#{props.pokemon.id}</h2>
+                    <h1 className={styles.name}>{props.pokemon.name}</h1>
                   </div>
-              </CardActionArea>
-      </Card>
+      </div>
     )
 }
 
