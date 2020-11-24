995cbd7ae15ce249165dd05bb474e279c47ddea8
Jeff <jlgranof@icloud.com>
2020-11-24 12:02:18 -0600

    test for erin

--- client/src/Components/UserPokedexPage/EditPokemon.js
+++ client/src/Components/UserPokedexPage/EditPokemon.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { fetchUserPokedex, updatePokemon } from '../../Redux/Actions/userPokedex'
 import { fetchAllPokedex, fetchBluePokedex, fetchRedPokedex, fetchYellowPokedex } from '../../Redux/Actions/pokedex'
 
